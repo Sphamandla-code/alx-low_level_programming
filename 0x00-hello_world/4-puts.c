5f5ee67752b19188390dc1836d87f973a8d6e8d6
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-08 14:50:49 +0200

    Low level prog

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-   char str1[] = "\"Programming is like building a multilingual puzzle";
+int main(void)
+{
+char str1[] = "\"Programming is like building a multilingual puzzle";
 
-   puts(str1);
+puts(str1);
    
-   return (0);
+return (0);
 }
