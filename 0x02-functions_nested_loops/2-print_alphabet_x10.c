4023ff9b5ff48f7a45553d69ddf43ac0ef20c24f
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-13 11:50:45 -0700

    ALX

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet lowercase 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char letter;
+
+	while (i++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
