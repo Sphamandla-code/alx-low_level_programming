053b79674613d820f8a93639c9eddc4e3bf9d53c
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-13 07:37:27 -0700

    functions and loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * Print _putchar
+ * main - Print _putchar
  *
- * Return 0
+ * Return: always 0
  */
 int main(void)
 {
@@ -15,6 +15,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
