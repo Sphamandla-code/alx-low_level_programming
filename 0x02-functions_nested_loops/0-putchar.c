7e6e654b20e8e0d4e4157e2363b9f4f0f0649d8b
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-13 07:24:01 -0700

    functions and loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <unistd>
+#include "main.h"
 
 /**
  * Print _putchar
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef main
+#define main
+
+/**
+ * File to import from
+ * all needed header files
+ */
+
+int _putchar(char c);
+
+#endif
