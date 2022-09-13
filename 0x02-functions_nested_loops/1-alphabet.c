4ea262b489ad1ba08410144e4eb38928c2e64f03
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-13 11:21:41 -0700

    func

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print alphabet lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void){
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
