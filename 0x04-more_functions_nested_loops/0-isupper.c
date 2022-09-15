13d736a0cca955ef9e2d5b731065bd79e8e976d5
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-15 11:05:05 +0200

    more funcetions and loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - check upper
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * FILE: MAIN.H
+ * IMPORT FROM HERE
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+
+int _isupper(int c);
+
+#endif
