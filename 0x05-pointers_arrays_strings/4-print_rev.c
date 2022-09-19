322c94593f25e97ea264d1d3f5ee5702475a449b
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-19 09:47:38 -0400

    Pointers arrays Strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,11 @@
 void print_rev(char *s)
 {
 	int i, len;
-	len = strlen(s);
 
-	for (i = len - 1; i >= 0; i--){
+	len = strlen(s);
+	
+	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
-	}
+
 	_putchar('\n');
 }
