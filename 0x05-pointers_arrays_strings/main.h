15421a9486456abe1fcb423d5ac0c806d67397cc
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-19 08:01:54 -0400

    Pointers arrays Strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,11 +7,11 @@
  */
 #include <stdlib.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-int _putchar(char c);
 
 #endif
