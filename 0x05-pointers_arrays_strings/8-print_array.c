fdce2093b30895dab566c0243bbde58c35a36d6f
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-19 08:54:31 -0400

    Pointers arrays Strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - input num of ele
+ * @a: array
+ * @b: elements to print
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d",a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		print(", ");
+	}
+
+	printf("\n");
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy
+ * @dest: to copy
+ * @src: source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
