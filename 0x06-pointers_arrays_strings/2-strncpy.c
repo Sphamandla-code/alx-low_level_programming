7e93b6ecec140c77dcd372e4a09c37cd0c9027d0
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-21 13:58:00 -0400

    Pointers arrays Strings2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,15 +6,17 @@
  * @dest: param 1
  * @src: param 2
  * @n: param 3
+ @Return: param 4
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	size_t i;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
+
 	for ( ; i < n; i++)
 		dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
