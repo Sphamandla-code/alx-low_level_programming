fbc3084b92e3956cac17808a261e15b5032ab730
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-09-21 14:14:34 -0400

    Pointers arrays Strings2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,5 +17,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif
