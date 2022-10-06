d8b09ecb7b838f742bdb74a790469d142130a1a7
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-10-06 10:38:05 -0400

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -18,7 +18,7 @@ prinf("Error\n");
 exit(98);
 }
 
-for (i =1; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
 {
@@ -30,7 +30,7 @@ exit(98);
 }
 }
 mul = atol(argv[1] *atol(argv[2]);
-print("%lu\n", mul);
+printf("%lu\n", mul);
 
 return (0);
 }
