27e1b9e59f3fb81f69d5f2ffc88bd2b9d6fe7e23
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-10-12 10:41:12 -0400

    0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name.
- * @name: The name to be printed.
- * @f: A pointer
+ *print_name - print a name
+ *@name: pointer to name
+ *@f: function pointer
+ *Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
