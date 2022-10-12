84ec1a0e65d9cc3e04cd1f99522faf448c9cdfff
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-10-12 08:45:42 -0400

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,47 @@
+#include "function_pointers.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints opcodes
+*@argc: arguments passed to program
+*@argv: array of arguments
+*
+*Return: 1 or 2
+*/
+
+int main(int argc, char *argv[])
+{
+
+int index, bytes;
+int (*address)(int, char **) = main;
+unsigned char opcode;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+
+bytes = atoi(argv[1]);
+
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+for (index = 0; index <  bytes; index++)
+{
+opcode = *(unsigned char *)address;
+printf("%.2x", opcode);
+
+if (index == bytes - 1)
+continue;
+printf(" ");
+address++;
+}
+printf("\n");
+return (0);
+}
