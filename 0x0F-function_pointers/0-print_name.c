c789efa71748b7af16dbb5b9fc7f0764649ce562
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-10-12 10:17:32 -0400

    0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,14 +4,11 @@
 *print_name - prints a name
 *@name:name to print
 *@f: function which print name
+*Return: nun
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-
-if (name == NULL || f == NULL)
-return;
-
-f(name);
-
+if (name && f)
+	f(name);
 }
