566036f73f4cd13aa59007f06f1d4c6a5de36910
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-10-13 07:42:04 -0400

    0x10-variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
 /**
  * sum_them_all - Returns the sum of all its paramters.
  * @n: The number of paramters passed to the function.
- 
+ * @...: param
  * Return: If n == 0 - 0.
  *         Otherwise - the sum of all parameters.
  */
