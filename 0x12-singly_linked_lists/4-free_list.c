bf3a16136381e08671fc833cbbe8a32a4ba5d315
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-10-20 15:08:28 +0200

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.c"
+
+/**
+ * free_list - free the list
+ * @head: head
+ * Return: 0
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
