b6736395acc09b35a9010a5ef2baa73d38ce7d00
Sphamandla-code <nkambulesphamandla@gmail.com>
2022-10-27 14:29:59 +0200

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - value of a bit
+ * @n: num 1
+ * @index: num 2
+ * Return: value
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
